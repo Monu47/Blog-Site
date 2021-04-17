b7ae11a7fbc041703274897f9ec61d9d0b1638a6
monumeena <monumeena@cse.iitb.ac.in>
2021-04-17 19:48:07 +0530

    here are files

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient,HttpParams,HttpResponse } from '@angular/common/http';
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  ngOnInit(): void {
+
+  }
+
+  
+
+}
